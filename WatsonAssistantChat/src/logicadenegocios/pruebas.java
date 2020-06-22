89e952527970e478b881b4956a52e7a92fc3ff7f
geromwatson <42458359+geromwatson@users.noreply.github.com>
2020-06-21 20:09:07 -0600

    pruebas

---
+++ WatsonAssistantChat/src/logicadenegocios/pruebas.java
@@ -0,0 +1,34 @@
+package Algorithms;
+
+
+
+public class pruebas {
+
+	
+
+	public static void main(String[] args) {
+		
+		
+		//Prueba Cesar	
+				
+		new Cesar("hola como estan",0,"cesar"); //lspd gsqs iwxdr
+		new Cesar("krod frpr hvwdq",1,"cesar"); //hola 
+		
+		//Prueba Phone Codification
+		new Phone_Code("password abc",0,"phone_code");
+		new Phone_Code("71 21 74 74 91 63 73 31 *21 22 23",1,"phone_code");
+
+
+		//Prueba Binary Codification
+		new Binary_Codification("sae que pa",0,"bin");
+		new Binary_Codification("10010 00000 00100 *10000 10100 00100 *01111 00000",1,"bin");
+
+		//Prueba Reverse Message
+		new Reverse_Message("codifiqueme esta",0,"reverse");
+	 
+
+				
+
+	}
+
+}
